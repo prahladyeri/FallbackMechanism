02de94776b34f0eb5b6c96c6c86472209ca33b05
Prahlad Yeri <prahladyeri@yahoo.com>
2025-01-12 15:56:00 +0530

    chore: init repo

---
+++ FallbackMechanism/GUI.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FallbackMechanism
+{
+    public partial class GUI : Form
+    {
+        int criticalValue = 50;
+        ContextMenu trayMenu;
+        bool warningShown = false;
+
+        public GUI()
+        {
+            InitializeComponent();
+
+            // Initialize system tray icon and context menu
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Open", ShowFormAgain);
+            trayMenu.MenuItems.Add("Exit", ExitApplication);
+
+            //this.Icon = null;
+            this.Icon = FallbackMechanism.Properties.Resources.logo_gmp ;
+            //this.Icon = Icon.FromHandle(FallbackMechanism.Properties.Resources.logo_gmp.GetHicon());
+            //this.Icon = new Icon(@"D:\source\csharp\FallbackMechanism\FallbackMechanism\images\logo-gmp.ico", 40, 40);
+
+            notifyIcon1.Icon = null;
+            notifyIcon1.Icon = this.Icon;
+            //notifyIcon1.Icon = FallbackMechanism.Properties.Resources.logo_maroon_gmp;
+            //notifyIcon1.Icon = Icon.FromHandle(FallbackMechanism.Properties.Resources.logo.GetHicon());
+            notifyIcon1.Visible = true;
+            notifyIcon1.ContextMenu = trayMenu;
+            notifyIcon1.Text = this.Text;
+        }
+
+        private void ShowFormAgain(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void ExitApplication(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            Application.Exit();
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            this.timer1.Interval = 1000 * 60 * 5; // 5 minutes
+            this.timer1.Enabled = true;
+            CheckBatteryStatus();
+        }
+
+        private void CheckBatteryStatus()
+        {
+            // Retrieve battery information
+            PowerStatus powerStatus = SystemInformation.PowerStatus;
+            // Check if the battery is discharging and below 50%
+            if (powerStatus.PowerLineStatus == PowerLineStatus.Offline && powerStatus.BatteryChargeStatus != BatteryChargeStatus.Charging) // If it's discharging
+            {
+                double per = this.criticalValue / 100.0;
+                if (powerStatus.BatteryLifePercent <= per)
+                {
+                    if (!warningShown) // Show warning only once
+                    {
+                        var ss = string.Format("Warning: Battery is discharging and is below {0}%.", criticalValue);
+                        notifyIcon1.Icon = null;
+                        notifyIcon1.Icon = FallbackMechanism.Properties.Resources.logo_maroon_gmp;
+                        notifyIcon1.ShowBalloonTip(3000, this.Text, ss, ToolTipIcon.Info);
+                        warningShown = true;
+                    }
+                }
+                else {
+                    warningShown = false;
+                }
+            }
+        }
+
+
+        private void GUI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            ShowFormAgain(sender, e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CheckBatteryStatus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.criticalValue =  Convert.ToInt32(this.textBox1.Text);
+            this.Hide();
+            this.notifyIcon1.ShowBalloonTip(2000, this.Text, "Value updated.", ToolTipIcon.Info);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # FallbackMechanism
 A Fallback Mechanism for Dell Laptops with Battery Amnesia
+
+## Genesis
+
+**FallbackMechanism** is a Windows program to solve a very specific problem. Many refurbished Dell Laptops these days, including the Latitude 7490 which I bought recently, ship with a very specific battery defect: There is nothing wrong with the battery itself which gives a decent 4-5 hours of runtime; however, the controller or bios firmware reports the incorrect percentage to Windows which results in your laptop shutting down much sooner at 43-45%. The charge is over at this point, but Windows thinks there are still few hours left.
+
+The official or recommended path to proceed here is to get a new battery, of course. But in case you don't have the budget for a new battery, `FallbackMechanism` is a software solution that will let you continue using an otherwise fine machine with no other issues except for this little "battery amnesia".
+
+Once you start the program, it stays in the system tray and warns you with a notification when the battery discharges below the set percentage value (50% by default). Mind you, the battery only has about 7-10% charge at this point and it's time to either shut the machine down or plug it to a charger.
+
+![tray-icon](https://raw.githubusercontent.com/prahladyeri/fallbackmechanism/main/screenshots/tray-icon.png)
+
+![program](https://raw.githubusercontent.com/prahladyeri/fallbackmechanism/main/screenshots/program.png)
+
+## Installation
+
+Just download the latest release [from here](https://github.com/prahladyeri/FallbackMechanism/releases/latest) and copy it to a folder in your C:\ drive. It's highly recommended to set it to run at startup for regular use.
+
+## Memory usage
+
+On my 16GB RAM Latitude 7490, it consumes a mere 3MB of memory! FallbackMechanism is pure .NET code (C#/CLR) and built with Visual Studio, it thus follows the most efficient workflow.
